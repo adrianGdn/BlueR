031d292f03779334268cd519742fe959f9bbde3a
adrianGdn <adrian.gandon@epsi.fr>
2017-10-30 16:39:21 +0100

    MAJ class object

--- BlueR/objectBatch/Device.cs
+++ BlueR/objectBatch/Device.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 /// objectBatch will contain all the object that we will need for this software.
 /// </summary>
 namespace objectBatch
-{//New Branch
+{
     /// <summary>
     /// Device is an object that will register informations about a device connected to this software in Bluetooth.
     /// </summary>
@@ -24,23 +24,23 @@ namespace objectBatch
         /// <summary>
         /// Allow you to create a Device object.
         /// </summary>
-        /// <param name="idDevice">The id of the device, for find it. Is an int.</param>
-        /// <param name="deviceName">The name of the device, for show it in a form. Is a string.</param>
-        /// <param name="idBluetooth">The address of the device, for send differents things. Is a string.</param>
+        /// <param name="idDevice">The id of the device, for find it. It's an int.</param>
+        /// <param name="deviceName">The name of the device, for show it in a form. It's a string.</param>
+        /// <param name="idBluetooth">The Bluetooth ID of the device, for send differents things. It's a string.</param>
         /// <param name="addressMail">The mail address of the user of the device. This is a string.</param>
         public Device(int idDevice, string deviceName, string idBluetooth, string addressMail)
         {
             this.setIdDevice(idDevice);
             this.setDeviceName(deviceName);
-            this.setDeviceAddress(idBluetooth);
+            this.setIdBluetooth(idBluetooth);
             this.setAddressMail(addressMail);
         }
         #endregion
         #region Getters
         /// <summary>
-        /// Allow you to get the device id.
+        /// Allow you to get the device ID.
         /// </summary>
-        /// <returns>Return the current ID of that device. Is an int.</returns>
+        /// <returns>Return the current ID of that device. It's an int.</returns>
         public int getIdDevice()
         {
             return this.idDevice;
@@ -48,16 +48,16 @@ namespace objectBatch
         /// <summary>
         /// Allow you to get the device name.
         /// </summary>
-        /// <returns>Return the current name of that device. Is a string.</returns>
+        /// <returns>Return the current name of that device. It's a string.</returns>
         public string getDeviceName()
         {
             return this.deviceName;
         }
         /// <summary>
-        /// Allow you to get the device address.
+        /// Allow you to get the device Bluetooth ID.
         /// </summary>
-        /// <returns>Return the current address of that device. Is a string.</returns>
-        public string getDeviceAddress()
+        /// <returns>Return the current Bluetooth ID of that device. It's a string.</returns>
+        public string getIdBluetooth()
         {
             return this.idBluetooth;
         }
@@ -74,7 +74,7 @@ namespace objectBatch
         /// <summary>
         /// Allow you to set the device ID.
         /// </summary>
-        /// <param name="idDevice">The id of the device, for find it. Is an int.</param>
+        /// <param name="idDevice">The id of the device, for find it. It's an int.</param>
         protected void setIdDevice(int idDevice)
         {
             this.idDevice = idDevice;
@@ -88,12 +88,12 @@ namespace objectBatch
             this.deviceName = deviceName;
         }
         /// <summary>
-        /// Allow you to set the device address.
+        /// Allow you to set the device Bluetooth ID.
         /// </summary>
-        /// <param name="deviceAddress">The address of the device, for send different things. Is a string.</param>
-        public void setDeviceAddress(string deviceAddress)
+        /// <param name="idBluetooth">The Bluetooth ID of the device, for send different things. It's a string.</param>
+        public void setIdBluetooth(string idBluetooth)
         {
-            this.idBluetooth = deviceAddress;
+            this.idBluetooth = idBluetooth;
         }
         /// <summary>
         /// Allow you to set the user mail address of the device.
@@ -105,6 +105,18 @@ namespace objectBatch
         }
         #endregion
         #region Methods
+        /// <summary>
+        /// Allow you to get a string that contain few attribute of that class.
+        /// </summary>
+        /// <returns>Return a sentence with few attribute.</returns>
+        public override string ToString()
+        {
+            string msg;
+            msg = "Le nom de l'appareil est : " + this.getDeviceName()
+                + ", l'adresse mail associé à cette appareil est : " + this.getIdBluetooth()
+                + ", l'identifiant BLuetooth de cette appareil est : ";            
+            return msg;
+        }
         #endregion
     }
 }
