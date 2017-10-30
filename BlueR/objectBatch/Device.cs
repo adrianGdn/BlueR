253d7c3b8d9fc77148ea526cf67aa46fa4f18805
adrianGdn <adrian.gandon@epsi.fr>
2017-10-30 15:23:53 +0100

    Add somes values

--- BlueR/objectBatch/Device.cs
+++ BlueR/objectBatch/Device.cs
@@ -16,7 +16,9 @@ namespace objectBatch
     {
         int idDevice;
         string deviceName;
-        string deviceAddress;
+        string idBluetooth;
+        string addressMail;
+
 
         #region Constructors
         /// <summary>
@@ -24,12 +26,14 @@ namespace objectBatch
         /// </summary>
         /// <param name="idDevice">The id of the device, for find it. Is an int.</param>
         /// <param name="deviceName">The name of the device, for show it in a form. Is a string.</param>
-        /// <param name="deviceAddress">The address of the device, for send differents things. Is a string.</param>
-        public Device(int idDevice, string deviceName, string deviceAddress)
+        /// <param name="idBluetooth">The address of the device, for send differents things. Is a string.</param>
+        /// <param name="addressMail">The mail address of the user of the device. This is a string.</param>
+        public Device(int idDevice, string deviceName, string idBluetooth, string addressMail)
         {
             this.setIdDevice(idDevice);
             this.setDeviceName(deviceName);
-            this.setDeviceAddress(deviceAddress);
+            this.setDeviceAddress(idBluetooth);
+            this.setAddressMail(addressMail);
         }
         #endregion
         #region Getters
@@ -55,7 +59,15 @@ namespace objectBatch
         /// <returns>Return the current address of that device. Is a string.</returns>
         public string getDeviceAddress()
         {
-            return this.deviceAddress;
+            return this.idBluetooth;
+        }
+        /// <summary>
+        /// Allow you to get the user mail address of the device.
+        /// </summary>
+        /// <returns>The mail address of the user of the device. This is a string.</returns>
+        public string getAddressMail()
+        {
+            return this.addressMail;
         }
         #endregion
         #region Setters
@@ -81,7 +93,15 @@ namespace objectBatch
         /// <param name="deviceAddress">The address of the device, for send different things. Is a string.</param>
         public void setDeviceAddress(string deviceAddress)
         {
-            this.deviceAddress = deviceAddress;
+            this.idBluetooth = deviceAddress;
+        }
+        /// <summary>
+        /// Allow you to set the user mail address of the device.
+        /// </summary>
+        /// <param name="addressMail">The mail address of the user of the device. This is a string.</param>
+        public void setAddressMail(string addressMail)
+        {
+            this.addressMail = addressMail;
         }
         #endregion
         #region Methods
