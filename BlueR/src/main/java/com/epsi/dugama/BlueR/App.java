5fdd08a211b406f538b95d7c0a9f126a8f4de872
adrianGdn <adrian.gandon@epsi.fr>
2017-11-03 19:41:01 +0100

    Init Java project.

---
+++ BlueR/src/main/java/com/epsi/dugama/BlueR/App.java
@@ -0,0 +1,13 @@
+package com.epsi.dugama.BlueR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
