aed2968a9a082705202e6c11b9025e439603010a
adrianGdn <adrian.gandon@epsi.fr>
2017-11-08 20:08:47 +0100

    Adding getDevice on DB

--- BlueR/src/main/java/com/epsi/dugama/BlueR/DAO.java
+++ BlueR/src/main/java/com/epsi/dugama/BlueR/DAO.java
@@ -1,5 +1,7 @@
 package com.epsi.dugama.BlueR;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import com.mysql.jdbc.Connection;
@@ -14,6 +16,9 @@ import com.mysql.jdbc.Statement;
  */
 public class DAO {
 	
+	/**
+	 * Method that allow us to test easily the connection with the DB
+	 */
 	public static void testCoBDD() {
 		String url = "jdbc:mysql://localhost/bluer";
 		String login = "root";
@@ -49,6 +54,52 @@ public class DAO {
 				e.printStackTrace();
 			}
 		}	
+	}
+	
+	/**
+	 * Method that allow us to get the device which was present on DB
+	 * 
+	 * @return List<Device> This is a list with the device which was present on DB
+	 */
+	public static List<Device> getDevices() {
+		String url = "jdbc:mysql://localhost/bluer";
+		String login = "root";
+		String mdp = "";
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
 		
+		List<Device> devices = new ArrayList<Device>();
+		Device aDevice = null;
+		
+		try {
+			// Step 1 : loading the driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Step 2 : retrieval of the connection
+			cn = (Connection) DriverManager.getConnection(url, login, mdp);
+			// Step 3 : creation of a statement
+			st = (Statement) cn.createStatement();
+			String sql = "SELECT * FROM device;";
+			// Step 4 : query execution
+			rs = (ResultSet) st.executeQuery(sql);
+			// Step 5 : We travel "ResultSet"
+			while (rs.next()) {
+				aDevice = new Device(rs.getString("deviceName"), rs.getString("idBluetooth"), rs.getString("mailAddress"));
+				devices.add(aDevice);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e2) {
+			e2.printStackTrace();
+		} finally {
+			try {
+				// Step 6 : liberation of the memory
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return devices;	
 	}
 }
